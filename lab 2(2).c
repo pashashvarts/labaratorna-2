3161e7785f3d933b5751d9c6aec2488d05ef8523
pashashvarts <78414643+pashashvarts@users.noreply.github.com>
2021-02-21 10:02:01 +0200

    Create lab 2(2).c

---
+++ lab 2(2).c
@@ -0,0 +1,13 @@
+#include <stdio.h>//pidkluchenya bibliotek
+#include<locale.h>
+#include<math.h>
+int main (void) {
+float a = 7;
+float b = 12;
+float c = -21;
+float y;//Buraz
+y = (a-b)/(2*b+c)+(1)/(a);
+printf("\ty\n");
+printf("\t%f\n",y);
+return 0;
+}
